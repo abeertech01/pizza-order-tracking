c98013c292426ec07b5c79fcbd0d57248e01176d
Abeer <abeer.technology@gmail.com>
2022-03-21 13:23:40 +0600

    authentication

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const flash = require("express-flash");
 const MongoDbStore = require("connect-mongo")(session);
+const passport = require("passport");
 
 // Database connection
 const url = "mongodb://localhost/pizza-order";
@@ -43,15 +44,23 @@ app.use(
   })
 );
 
+// Passport config
+const passportInit = require("./app/config/passport");
+passportInit(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash());
 
 // Assets
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Global middleware
 app.use((req, res, next) => {
   res.locals.session = req.session;
+  res.locals.user = req.user;
   next();
 });
 // set Template engine
